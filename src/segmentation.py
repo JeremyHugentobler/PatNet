50a4142b950e69074cd3110ee675f8fd93861665
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2025-12-13 12:55:14 +0100

    Final visulaization tool

--- src/segmentation.py
+++ src/segmentation.py
@@ -68,13 +68,20 @@ def show_single_mask(image, mask):
   
   
 def show_match(image1, mask1, image2, mask2):
-    # We want to display on a single figure per match the two image side by side, where the background is a bit darkened
-    label3_1 = np.stack([mask1[0],mask1[0],mask1[0]], axis=2)
-    label3_2 = np.stack([mask2[0],mask2[0],mask2[0]], axis=2)
-    r1 = image1 * label3_1 + image1 * (1-label3_1) * 0.3
-    r2 = image2 * label3_2 + image2 * (1-label3_2) * 0.3
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-    axs[0].imshow(r1)
-    axs[1].imshow(r2)
-    plt.show()
+  image1 = np.array(image1)
+  image2 = np.array(image2)
+
+  # We want to display on a single figure per match the two image side by side
+  # where the background is a bit darkened
+  label3_1 = np.stack([mask1[0], mask1[0], mask1[0]], axis=2)
+  label3_2 = np.stack([mask2[0], mask2[0], mask2[0]], axis=2)
+  
+  # Calculate and cast to uint8 to fit the [0..255] range for integers
+  r1 = (image1 * label3_1 + image1 * (1 - label3_1) * 0.3).astype(np.uint8)
+  r2 = (image2 * label3_2 + image2 * (1 - label3_2) * 0.3).astype(np.uint8)
+  
+  fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+  axs[0].imshow(r1)
+  axs[1].imshow(r2)
+  plt.show()
     
